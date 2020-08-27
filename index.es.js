35af965392060a03da9ebc6ed3d330a9c5e548a1
exuanbo <exuanbo@protonmail.com>
2020-08-27 16:59:55 +0800

    feat: add es module

---
+++ index.es.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import path from 'path';
+import through from 'through2';
+import PluginError from 'plugin-error';
+
+const PLUGIN_NAME = '@exuanbo/gulp-inject-inline';
+
+const injectInline = () => {
+  return through.obj((file, _, callback) => {
+    if (file.isNull()) {
+      return callback(null, file)
+    }
+
+    if (file.isStream()) {
+      return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported'))
+    }
+
+    const regex = /(?:<!--|\/\*)\s*?inject-inline:\s*?([^\s].+?)\s*?(?:-->|\*\/)/gi;
+
+    const newFileContents = String(file.contents).replace(
+      regex,
+      (_match, src) => {
+        const sourcePath = src.startsWith('/')
+          ? path.join(process.cwd(), src)
+          : path.join(file.dirname, src);
+        return String(fs.readFileSync(sourcePath))
+      }
+    );
+
+    file.contents = Buffer.from(newFileContents);
+    callback(null, file);
+  })
+};
+
+var gulpInjectInline = injectInline;
+
+export default gulpInjectInline;
